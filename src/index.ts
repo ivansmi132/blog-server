e058d849d39e14c1cedf2c60ba693e8c6a441e6c
ivansm <ivansmirnov132@gmail.com>
2024-02-13 01:02:09 +0200

    added cors to support render cloud

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,11 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors({
-    origin: "http://127.0.0.1:3000",
+    origin: [
+        "http://127.0.0.1:3000",
+        "https://react-client-d2vl.onrender.com",
+        "postgres://blog_db_1j4c_user:WkcI98YhwNXIXpfywi3ifawITOrJjpEd@dpg-cn57tmol6cac73c7ee40-a/blog_db_1j4c"
+    ],
     credentials: true
 }))
 
